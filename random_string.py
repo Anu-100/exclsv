e30f14ca0d7ce252804715cbb12bccd1a04c5399
ANUSREE <anusreemitra47@gmail.com>
2024-11-27 13:08:28 +0530

    removed unnecessary file

--- random_string.py
+++
@@ -1,8 +0,0 @@
-import string
-import random
-
-def generate_secret_key():
-    chars = string.digits + string.ascii_lowercase + string.ascii_uppercase
-    return ''.join(random.choice(chars) for _ in range(50))
-
-print(generate_secret_key())
