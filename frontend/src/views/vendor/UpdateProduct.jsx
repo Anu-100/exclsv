cada326c3521f13f939719185a518f7f5fbda37e
ANUSREE <anusreemitra47@gmail.com>
2024-12-06 18:33:40 +0530

    edited updateProduct.jsx

--- frontend/src/views/vendor/UpdateProduct.jsx
+++ frontend/src/views/vendor/UpdateProduct.jsx
@@ -142,25 +142,7 @@ const UpdateProduct = () => {
             }
         }
         setGallery(newGallery);
-    }
-
-    const getCookie = (name) => {
-        let cookieValue = null;
-        if (document.cookie && document.cookie !== '') {
-            const cookies = document.cookie.split(';');
-            for (let i = 0; i < cookies.length; i++) {
-                const cookie = cookies[i].trim();
-                if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
-    };
-    
-    const csrftoken = getCookie('csrftoken');
-    
+    }    
 
     const handleProductSubmit = async (e) => {
         e.preventDefault();
@@ -204,7 +186,6 @@ const UpdateProduct = () => {
             await apiInstance.patch(`vendor/update-product/${userData?.vendor_id}/${params.pid}/`, formData, {
                 headers:{
                     'Content-Type': 'multipart/form-data',
-                    'X-CSRFToken': csrftoken
                 }
             })
             Toast.fire({
